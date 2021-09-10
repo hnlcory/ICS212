73d82a4ee5135b4231870671cafb28904903be7a
Cory Parker <school.coryparker@gmail.com>
2021-09-09 20:29:43 -1000

    Add files via upload

--- homework2/homework2.c
+++ homework2/homework2.c
@@ -8,14 +8,15 @@
 //
 //  INSTRUCTOR:  Cory Parker
 //
-//  DATE:        September 8, 2021
+//  DATE:        September 9, 2021
 //
 //  FILE:        homework2.c
 //
-//  DESCRIPTION:
-//   Describe the file
-//
+//  DESCRIPTION: A program that prints out numbers given and
+//               prints out if they are a multiple of 3.
+//   
 ****************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -23,20 +24,21 @@ int userInterface(void);
 void print_table(int);
 int is_multiple3(int);
 int clean_stdin();
+
 /*****************************************************************
 //
 //  Function name: main
 //
-//  DESCRIPTION:   Don't forget to describe what your main
-//                 functions does.
+//  DESCRIPTION:   Calls the print_table function with the number
+//                 given in userInterface.
+//                 
 //
 //  Parameters:    argc (int) : The number of elements in argv
 //                 argv (char*[]) : An array of arguments passed
 //                                  to the program.
 //
-//  Return values:  0 : some meaning
-//                 -1 : some meaning
-//
+//  Return values:  0 : program completed.
+//                 
 ****************************************************************/
 
 int main(int argc, char* argv[])
@@ -49,14 +51,16 @@ int main(int argc, char* argv[])
 //
 //  Function name: userInterface
 //
-//  DESCRIPTION:   A template function
-//                 This function does not do anything.
-//                 Please describe your function correctly.
+//  DESCRIPTION:   Prints an intro message, asks for an int
+//                 and returns the gained value.
+//                 
 //
-//  Parameters:    bar (int) : Describe the meaning
+//  Parameters:    valid (int) : used to check if user imput
+//                 is valid (1) or invalid (0).
 //
-//  Return values:  0 : some meaning
-//                 -1 : some meaning
+//                 input (int) : the int given from user.
+//
+//  Return values: input : int given from user.
 //
 ****************************************************************/
 
@@ -67,11 +71,13 @@ int userInterface(void)
   printf("This is a simple program to generate s table of numbers from 0 to \n the maximum number given. It will also show if it is a multiple of 3.\n"); 
   valid=0;
 
-  while(valid !=1){
+  while(valid !=1)
+  {
     printf("Enter a max number:\n");
     valid = scanf("%d", &input);
 
-    if (valid<1 || input <= 0){
+    if (valid<1 || input <= 0)
+    {
       clean_stdin();
       printf("error, please enter valid input");
       valid=0;
@@ -79,80 +85,75 @@ int userInterface(void)
  
   }
     
-   return input; 
+  return input; 
 }
-
 
 /*****************************************************************
 //
 //  Function name: clean_stdin
 //
-//  DESCRIPTION:   A template function
-//                 This function does not do anything.
-//                 Please describe your function correctly.
+//  DESCRIPTION:   Catches possible char inputs in userInterface.
+//                 Cleans stdin in event of.
 //
-//  Parameters:    bar (int) : Describe the meaning
-//
-//  Return values:  0 : some meaning
-//                 -1 : some meaning
+//  Return values:  1 : there is a character caught with getchar.
 //
 ****************************************************************/
 
 int clean_stdin()
 {
-    while (getchar()!='\n');
-    return 1;
+  while (getchar()!='\n');
+  return 1;
 }
 
 /*******************************************************************
 //
-//   Function name: is_multiple3
+//  Function name: is_multiple3
 //
-//  DESCRIPTION:   A template function
-//                 This function does not do anything.
-//                 Please describe your function correctly.
+//  DESCRIPTION:   Checks given int to see if it is a multiple of 3.
 //
-//  Parameters:    bar (int) : Describe the meaning
+//  Parameters:    inp (int) : The int given when called.
 //
-//  Return values:  0 : some meaning
-//                 -1 : some meaning
+//  Return values:  0 : not a multiple of 3.
+//                  1 : is a multiple of 3.
 //
 *******************************************************************/
+
 int is_multiple3(int inp)
 {
-
-  if (inp % 3 ==0){
+  if (inp%3==0)
+  {
     return 1;
   }  
   return 0;
 }
+
 /*****************************************************************
 //
 //  Function name: print_table
 //
-//  DESCRIPTION:   A template function
-//                 This function does not do anything.
-//                 Please describe your function correctly.
+//  DESCRIPTION:   Uses a for-loop to generate a table, starting
+//                 from 0 to the given maximum number, with the 
+//                 additional column.
 //
-//  Parameters:    bar (int) : Describe the meaning
-//
-//  Return values:  0 : some meaning
-//                 -1 : some meaning
+//  Parameters:    i (int) : Used to loop from 0 to inp.
 //
 ****************************************************************/
-void print_table(int inp){
+
+void print_table(int inp)
+{
   int i;
 
   printf("Number  Multiple of 3?\n");
-  for (i=0;i<=inp;i=i+1){
+  for (i=0;i<=inp;i=i+1)
+  {
     printf("%*i",6,i);
-    if(is_multiple3(i)==1){
+    if(is_multiple3(i)==1)
+    {
       printf("%*s\n",5,"Yes");  
     }
-    else{
+    else
+    {
       printf("%*s\n",4,"No");
     }  
-
   }
-
 }
