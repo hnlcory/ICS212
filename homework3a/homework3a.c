8f24289c947b932be32aa7e62d45a7884fd23646
Cory Parker <school.coryparker@gmail.com>
2021-09-15 17:54:16 -1000

    Add files via upload

--- homework3a/homework3a.c
+++ homework3a/homework3a.c
@@ -10,24 +10,32 @@ int main(int argc, char* argv[])
     float score1 = 20.5;
     char ca[3] = "Hi";
 
-    printf("before dummy call:\n");
+    printf("\n\nbefore dummy call:\n");
 
     printf("argc  %i\n",argc);
     /* printf("argv  %c\n",argv[]);*/
 
 
+    printf("variable num1 is at address: %p\n", (void*)&num1);
     printf("num1 %i\n",num1);
     printf("variable num2 is at address: %p\n", (void*)&num2);
     printf("num2 %i\n",num2);
 
-    printf("c1  %c\n",c1);
-    printf("c2  %c\n",c2);
 
+    printf("variable c1 is at address: %p\n", (void*)&c1);
+    printf("c1 %c\n",c1);
+    printf("variable c2 is at address: %p\n", (void*)&c2);
+    printf("c2 %c\n",c2);
+
+    printf("variable score1 is at address: %p\n", (void*)&score1);
     printf("score1 %f\n",score1);
 
+    printf("variable ca[0] is at address: %p\n", (void*)&ca[0]);
     printf("ca[0] %c\n",ca[0]); 
+    printf("variable ca[1] is at address: %p\n", (void*)&ca[1]);
     printf("ca[1] %c\n",ca[1]);
-    printf("ca[2] %i\n",ca[2]);
+    printf("variable ca[2] is at address: %p\n", (void*)&ca[2]);
+    printf("ca[2] %c\n",ca[2]);
 
 
 
@@ -40,9 +48,13 @@ void dummy(int x, char y, char* z, float w)
 {
  
     printf("\ndummy called:\n");
+    printf("variable x is at address: %p\n", (void*)&x);
     printf("x %i\n",x);
+    printf("variable y is at address: %p\n", (void*)&y);
     printf("y %c\n",y);
+    printf("variable z is at address: %p\n", (void*)&z);
     printf("z %s\n",z);
+    printf("variable w is at address: %p\n", (void*)&w);
     printf("w %f\n",w);
 
     x++;
@@ -51,8 +63,11 @@ void dummy(int x, char y, char* z, float w)
 
 
     printf("\npost dummy:\n");
+    printf("variable x is at address: %p\n", (void*)&x);
     printf("x %i\n",x);
+    printf("variable y is at address: %p\n", (void*)&y);
     printf("y %c\n",y);
+    printf("variable w is at address: %p\n", (void*)&w);
     printf("w %f\n",w);
     /* pause here */
 }
