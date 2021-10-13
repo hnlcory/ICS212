c5fa78ecd7e6d44cedf0be4dae9ea51859246205
Cory Parker <school.coryparker@gmail.com>
2021-10-12 20:16:41 -1000

    Add files via upload

--- homework5/iofunctions.c
+++ homework5/iofunctions.c
@@ -1,19 +1,20 @@
 /*****************************************************************
 //
-//  NAME:        Ravi Narayan
+//  NAME:        Cory Parker
 //
-//  HOMEWORK:    1
+//  HOMEWORK:    5
 //
 //  CLASS:       ICS 212
 //
 //  INSTRUCTOR:  Ravi Narayan
 //
-//  DATE:        September 3, 2112
+//  DATE:        October 12, 2021
 //
-//  FILE:        template.c
+//  FILE:        iofunctions.c
 //
-//  DESCRIPTION:
-//   Describe the file
+//  DESCRIPTION: holds functions to read and write pokemon
+//               to and from files. 
+//   
 //
 ****************************************************************/
 
@@ -38,29 +39,86 @@
 
 int main(int argc, char* argv[])
 {
+   
+   struct pokemon pArray[3] ={10,"Pikachu",30,"Ditto",7,"Pidgey"};
+   struct pokemon pArray2[2] ={85,"Weedle",6,"Weedle"};
 
-   struct pokemon pArray[3] = {10,"Pikachu",30,"Ditto",7,"Pidgey"};
-   struct pokemon pArray2[2] = {85,"Weedle",6,"Weedle"};
-  
+
+   struct pokemon pArrayW[2];
+   int j = 2;
+   int itr;
+   int help = sizeof(pArrayW)/sizeof(pArrayW[0]);
+   
    int inp;
    printf("\n1 or 2 array\n");
    fscanf(stdin, "%d", &inp);
+   
    if (inp == 1)
    {
-   printf("1");
    writefile(pArray,3,"output.txt");
    }
+   
    else
    {
-   printf("2");
    writefile(pArray2,2,"output.txt");
-   }    
+   }
+    
+     
+   readfile(pArrayW,&j,"output.txt");
+   
+
+   for (itr=0; itr < help; itr++)
+    {
+
+    printf("%i\n%s\n",pArrayW[itr].level,pArrayW[itr].name);
+
+    }  
    return 0;
 }
 
 /*****************************************************************
 //
-//  Function name: foo
+//  Function name: writefile
+//
+//  DESCRIPTION:   A function to write pokemon from an array
+//                 to a file.
+//
+//  Parameters:    pokearray (struct pokemon) : an aray that holds pokemon
+//                 num (int) : the number of pokemon
+//                 filename (char[]) : the name of the output file
+//
+//  Return values:  0 : sucessful write
+//                 -1 : unsucessful write
+//
+****************************************************************/
+
+int writefile( struct pokemon pokearray[ ], int num, char filename[ ] )
+{
+    int i = 0;
+    FILE * fileOut;
+    fileOut = fopen(filename,"w");
+
+    if (fileOut == NULL)
+    {
+        return -1;
+    }
+
+    else
+    {
+        do{
+            fprintf(fileOut,"%i\n%s\n",pokearray[i].level,pokearray[i].name);
+            i = i + 1;
+        }
+        while (i < num);
+
+        fclose(fileOut);
+        return 0; 
+    }
+}
+
+/*****************************************************************
+//
+//  Function name: readfile
 //
 //  DESCRIPTION:   A template function
 //                 This function does not do anything.
@@ -73,50 +131,40 @@ int main(int argc, char* argv[])
 //
 ****************************************************************/
 
-int writefile( struct pokemon pokearray[ ], int num, char filename[ ] )
-{
-    int i=0;
-    FILE * fileOut;
-    fileOut = fopen(filename,"w");
-
-    if (fileOut == NULL)
-    {
-        return -1;
-    }
-
-    else
-    {
-   
-    /*check for data alrady in file*/  
-        while (i < num )
-        {
-            /*do the actual add stuff, adding bigger to smaller adds a 0?*/
-            fprintf(fileOut,"%i\n%s\n",pokearray[i].level,pokearray[i].name);
-            i++;
-        }
-        fclose(fileOut);
-        return 0;
-         
-    }
-
-}
-
 int readfile( struct pokemon pokearray[ ], int* num, char filename[ ] )
-{
-    FILE * fileOut;
-
-    if (fileOut == NULL)
-    {
+{ 
+    int i = 0;
+    int end = 0;
+    int tempLevel;
+    FILE * fileIn;
+    fileIn = fopen(filename,"r");    
+ 
+    if (fileIn == NULL)
+    {      
         return -1;
     }
   
     else
     {
-        return 0;
-
+        do
+        {
+            if (fscanf(fileIn, "%d\n", &tempLevel) != 1)
+            {
+                end = 1;
+                i = i-1;
+            }
+            
+            else
+            {
+                pokearray[i].level = tempLevel;
+                fgets(pokearray[i].name, 25,fileIn);
+                i++;
+            }
+        }
+        while (end == 0 && feof(fileIn) == 0);
     }
- 
+
+    *num = i;
+    fclose(fileIn);
+    return 0;    
 }
-
-
-
